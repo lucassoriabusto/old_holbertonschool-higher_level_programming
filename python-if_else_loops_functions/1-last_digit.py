b14261327127f284f1eecfff29ffc1fc9c23dba9
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-01-21 19:00:34 -0800

    1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number % 10 > 5:
-    print(f"Last digit of {number} is {number % 10} and is greater than 5")
-elif number % 10 == 0:
-    print(f"Last digit of {number} is {number % 10} and is 0")
-elif number % 10 < 6 and not -0:
-    print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
-elif number % 10 < 0:
-    print("Pasta Frola con memrillo")
+n = number % 10
+if n > 5:
+    print(f"Last digit of {number} is {n} and is greater than 5")
+if n == 0:
+    print(f"Last digit of {number} is {n} and is 0")
+if n < 6 and n != 0 and number > 0:
+    print(f"Last digit of {number} is {n} and is less than 6 and not 0")
+if n < 0:
+    np = number * -1
+    print(f"Last digit of {number} is {np % 10 * -1} and is less than 6 and not 0")
+#elif number % 10 < 0:
+#    print("Pasta Frola con memrillo")
 
 
