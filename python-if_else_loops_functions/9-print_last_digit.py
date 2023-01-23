5fb3fecf4503aba0ca4d0ee7a7895a69f33d52d4
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-01-22 17:15:59 -0800

    9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         np = number % -10
-        print(np, end="")
+        print(np * -1, end="")
         return np
     else:
         print(number % 10, end="")
