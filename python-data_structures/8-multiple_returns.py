7dae4bf9848b97ae52a94da17ea8d9027ddaead0
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-01-27 13:19:14 -0800

    Task 08

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        new_tupla = len(sentence), None
+        new_tupla = (len(sentence), None)
         return new_tupla
     else:
-        new_tupla = len(sentence), sentence[0]
+        new_tupla = (len(sentence), sentence[0])
         return new_tupla
