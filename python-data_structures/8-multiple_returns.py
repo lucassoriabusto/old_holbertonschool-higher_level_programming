ed3b827855a68ee1a25742adfc3470f3ecfc443c
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-01-27 13:44:53 -0800

    Task 08

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if sentence is None:
         return 0, None
-    if sentence =="":
+    if sentence == "":
         return 0, None
     else:
         return len(sentence), sentence[0]
