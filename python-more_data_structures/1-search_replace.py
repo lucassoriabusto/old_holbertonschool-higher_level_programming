e9f7bf770e025637c9c53fb501784c871ca44e68
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-01-28 17:26:19 -0800

    Task 01

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for n, ele in enumerate(my_list):
+        if ele == search:
+             new_list[n] = replace
+    return new_list
+
