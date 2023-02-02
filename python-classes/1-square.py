c405f37d5f7230ad392079081cc4fd93028c82a2
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-02 10:35:06 -0800

    Task 01

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines an empty class"""
+
+
+class Square:
+    """The case is defined as square"""
+    pass
+
+    def __init__(self, size):
+        """Defines a private instance attribute"""
+        self.__size = size
