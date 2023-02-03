5e0fe347cfc9b2901a568e5ac20ed710e30f34e4
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-03 08:00:57 -0800

    Task 03

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines an empty class"""
+
+
+class Square:
+    pass
+
+    def __init__(self, size=0):
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Returns the area of a square"""
+        return self.__size ** 2
