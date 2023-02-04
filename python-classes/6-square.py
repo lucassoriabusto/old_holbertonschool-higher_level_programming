2f25b3b9b5301b2daa48fb0b24cf45ddb49e01ff
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-03 19:42:29 -0800

    Task 06

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,6 +45,7 @@ class Square:
                 for y in range(self.__size):
                     print("#", end="")
                 print("")
+
     """@property:
         Permite axceder los atributos privados como si fueran públicos"""
     @property
@@ -54,17 +55,4 @@ class Square:
         Permite modificar los atributos privados"""
     @position.setter
     def position(self, value):
-
-        """if type(self.__position) != tuple or type(value) != int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value"""
-
-    """@position.setter
-    def position(self, value):
-        if len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0]) != int or type(value[1]) != int or len(value) != 2 
-        or value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = position"""
+        return self.__position
