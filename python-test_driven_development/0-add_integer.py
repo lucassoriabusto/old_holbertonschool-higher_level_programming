75d66c829e6cc783c339b53e06112078ac8c72e4
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-05 12:36:27 -0800

    Task 00

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Write a function that adds 2 integers."""
 def add_integer(a, b=98):
-    
+    """Check that a or b is an int or float."""
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
