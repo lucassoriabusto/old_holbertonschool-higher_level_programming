1fb7ab9bea6269b55ab4c425598798fcad57d3e0
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-07 15:21:57 -0800

    Task 03

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""Write a function that prints My name is <first name> <last name>"""
 
 
 def say_my_name(first_name, last_name=""):
+    """Check that both parameters are strings"""
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
