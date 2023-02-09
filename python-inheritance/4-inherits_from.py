22c6d5de1e18b265a9a380ad49910c8862117f36
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-09 10:25:22 -0800

    Task 04

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""g"""
+"""Function that returns True if the object is an
+instance of a class that inherited"""
 
 
 def inherits_from(obj, a_class):
-    """e"""
-     return isinstance(obj, a_class)
+    """isinstance:
+    Returns True if the specified object is of the specified type,
+    otherwise False
+    """
+    return isinstance(obj, a_class)
