3e68eff78fcb31be390d0de385adfd32ca56f2ef
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-11 21:16:05 -0800

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""d"""
+"""Function that reads a text file"""
 
 
 def read_file(filename=""):
+    """Reads a file with open() and then prints its contents"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
