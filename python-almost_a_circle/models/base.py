353d65cc35b0fe5f8aa63167f7a44bc3afff835c
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-14 10:28:53 -0800

    Task 01

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a Base class"""
+
+
+class Base:
+    __nb_objects = 0
+    """Private class attribute"""
+    def __init__(self, id=None):
+        """d"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
