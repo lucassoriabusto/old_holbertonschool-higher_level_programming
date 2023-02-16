f36543487ac9f9d3a52719a8d876fceb136a079b
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-15 18:57:48 -0800

    Task 05

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,10 +79,9 @@ class Rectangle(Base):
 
     def display(self): 
         """Prints in stdout the Rectangle instance with the character #"""
-        for n in range(self.__height):
-            for i in range(self.__x):
-                """print(" ", end="")"""
-            for j in range(self.__width):
+        for h in range(self.__height):
+            for x in range(self.__x):
+            for w in range(self.__width):
                 print("#", end="")
             print()
         
