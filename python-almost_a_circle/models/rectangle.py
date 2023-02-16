696412dfe5bc17a58a59e889db876d496a64a90d
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-16 11:46:44 -0800

    Task 05

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -84,5 +84,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """Update the class Rectangle
-        Con self.variable accedes al valor de la variable para poder retornarlo"""
-        return(f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
+        Con self.variable accedes al valor de la
+        variable para poder retornarlo"""
+        return(f"[Rectangle] ({self.id}) {self.x}/{self.y}
+        - {self.width}/{self.height}")
