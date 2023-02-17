8d15c369ee4d047f2b144b68dc1a7e628c781b30
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-02-17 12:33:30 -0800

    Task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """d"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        """self.width = size
+        self.height = size"""
+        self.size = size
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
