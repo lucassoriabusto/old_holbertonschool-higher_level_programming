24898c853a96327e7df4dfad59874c6c122d512d
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-03-15 15:03:20 -0700

    Task 10

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Script that lists all records of the table second_table.
 -- Records should be ordered by score (top first).
-SELECT * FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
