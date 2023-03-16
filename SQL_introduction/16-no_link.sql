5e84ded70ad2e786bb86a9d007cdf396f683b459
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-03-15 19:14:11 -0700

    Task 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Script that lists all records of the table second_table.
-SELECT name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
