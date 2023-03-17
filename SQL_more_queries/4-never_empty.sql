ec3667de2fa8685562d284978b112836441e574a
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-03-16 21:40:54 -0700

    Task 05

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,6 @@
 -- Script that creates the table id_not_null.
 -- id INT with the default value 1.
 CREATE TABLE IF NOT EXISTS id_not_null (
-	id INT NOT NULL DEFAULT 1,;
+	id INT NOT NULL DEFAULT1;
 	name VARCHAR(256)
 );
