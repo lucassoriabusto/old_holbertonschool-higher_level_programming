d66cba3c801c0f9e2832043c1f65c5184ceefe07
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-03-27 10:06:42 -0700

    14

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- 
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_shows_genres ON tv_genres.id = tv_shows_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_shows_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
