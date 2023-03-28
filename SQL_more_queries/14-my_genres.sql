e4259c427a5a55b074f2b17143c59c5a23741acf
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-03-27 20:51:13 -0700

    Task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- 
 SELECT tv_genres.name
-FROM tv_shows
+FROM tv_genres
 INNER JOIN tv_shows_genres ON tv_genres.id = tv_shows_genres.genre_id
 INNER JOIN tv_shows ON tv_shows.id = tv_shows_genres.show_id
 WHERE tv_shows.title = "Dexter"
