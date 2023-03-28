2a3bcd2ef77ed3905ac25b45bb1dc30abe085da8
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-03-27 21:10:46 -0700

    Task 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 --
-SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre_name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-LEFT JOIN show_genres ON tv_shows.id = show_genres.show_id
-LEFT JOIN tv_genres ON show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, genre_name ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
