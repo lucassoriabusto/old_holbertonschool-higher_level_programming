9ccf166233bfb377e80e788e870eb1b11e941c4d
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-03-27 20:49:29 -0700

    Task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- 
 SELECT tv_genres.name
 FROM tv_shows
-JOIN tv_shows_genres ON tv_genres.id = tv_shows_genres.genre_id
-JOIN tv_shows ON tv_shows.id = tv_shows_genres.show_id
-WHERE tv_shows.title = 'Dexter'
+INNER JOIN tv_shows_genres ON tv_genres.id = tv_shows_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_shows_genres.show_id
+WHERE tv_shows.title = "Dexter"
 ORDER BY tv_genres.name ASC;
